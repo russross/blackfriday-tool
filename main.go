620ae4182490ac8cfa8b1fcfd11c8f4802eb7c10
GRosenberg <gbrosenberg85@gmail.com>
2014-05-11 18:27:14 -0700

    Add support for header IDs

--- main.go
+++ main.go
@@ -120,6 +120,7 @@ func main() {
 	extensions |= blackfriday.EXTENSION_AUTOLINK
 	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
 	extensions |= blackfriday.EXTENSION_SPACE_HEADERS
+	extensions |= blackfriday.EXTENSION_HEADER_IDS
 
 	var renderer blackfriday.Renderer
 	if latex {
